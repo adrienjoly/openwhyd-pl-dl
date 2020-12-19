646b4973d8ad32e5d3469aeeab5f6f5341b0ca7b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-19 20:54:28 +0100

    fix(bug): fetch each playlist from its own URL

--- openwhyd-pl-dl-json.sh
+++ openwhyd-pl-dl-json.sh
@@ -12,10 +12,10 @@ jq -c '.[]' "playlists.json" \
     PL_URL=$(echo $PL | jq -r '.url') # extract playlist URL
     PL_NAME=$(echo $PL | jq -r '.name') # extract playlist name
     PL_NUM=$(echo $PL | jq -r '.id') # extract playlist number
-    PL_NUM=$(printf "%03d\n" $PL_NUM) # 0-padding
+    PL_NUM_PADDED=$(printf "%03d\n" $PL_NUM) # 0-padding
     PL_NAME=$(echo $PL_NAME | tr -dc '[:alnum:]\n\r') # remove special characters
-    PL_FILENAME="$PL_NUM-$PL_NAME"
+    PL_FILENAME="$PL_NUM_PADDED-$PL_NAME.json"
     echo "- $PL_URL => $PL_FILENAME"
     # echo "mkdir \"$PL_PATH\"; cd \"$PL_PATH\"; ../openwhyd-dl.sh \"https://openwhyd.org$PL_URL\"; cd .." >>$DOWNLOAD_SCRIPT
-    curl -sS "$PROFILE_URL?format=json&limit=999999" | jq . > "$PL_FILENAME.json"
+    curl -sS "${PROFILE_URL}/playlist/${PL_NUM}?format=json&limit=999999" | jq . > "${PL_FILENAME}"
 done;
