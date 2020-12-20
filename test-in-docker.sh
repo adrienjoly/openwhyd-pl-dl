f4ab1a51fa5c9cad67d96bcb730b05505a438ae7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-20 15:07:42 +0100

    ✅ second test

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -1,13 +1,13 @@
 set -e # this script will exit if any command returns a non-null value
 
 DEST_DIR="./tmp"
-mkdir -p "${DEST_DIR}"
-
 docker build --tag "openwhyd-pl-dl" .
 
 # 1. Test openwhyd-pl-dl-json.sh
 
 PROFILE_URL="https://openwhyd.org/test"
+
+mkdir -p "${DEST_DIR}"
 
 docker run -t --rm -v "${PWD}:/app" -w "/app" "openwhyd-pl-dl" \
            "/bin/bash" -c "./openwhyd-pl-dl-json.sh ${PROFILE_URL} ${DEST_DIR}"
@@ -19,6 +19,8 @@ rm -rf "${DEST_DIR}"
 # 2. Test openwhyd-dl.sh
 
 PLAYLIST_URL="https://openwhyd.org/test/playlist/2" # playlist with just 1 short mp3 file
+
+mkdir -p "${DEST_DIR}"
 
 docker run -t --rm -v "${PWD}:/app" -w "/app" "openwhyd-pl-dl" \
            "/bin/bash" -c "./openwhyd-dl.sh ${PLAYLIST_URL} ${DEST_DIR}"
