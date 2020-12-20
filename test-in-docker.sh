b1d2ca52858d049a613d1a1e362b64fd8a83a6af
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-20 14:48:18 +0100

    check playlists.json + cleanup

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -6,7 +6,12 @@ DEST_DIR="./tmp"
 mkdir -p "${DEST_DIR}"
 
 docker build --tag "openwhyd-pl-dl" .
+
 docker run -t --rm -v "${PWD}:/app" -w "/app" "openwhyd-pl-dl" \
            "/bin/bash" -c "./openwhyd-pl-dl-json.sh ${PROFILE_URL} ${DEST_DIR}"
+
+[ ! -f "${DEST_DIR}/playlists.json" ] && echo "❌ Missing: ${DEST_DIR}/playlists.json" && exit 1
+
+rm -rf "${DEST_DIR}"
 
 echo "✅ Done."
