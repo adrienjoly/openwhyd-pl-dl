f6d730fe678b4c773ca17b5241af543e0fc6e19f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-20 13:32:16 +0100

    fix "the input device is not a TTY" on CI: https://github.com/adrienjoly/openwhyd-pl-dl/runs/1584582287#step:3:32

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -3,7 +3,7 @@ set -e # this script will exit if any command returns a non-null value
 PROFILE_URL="https://openwhyd.org/test"
 
 docker build --tag "openwhyd-pl-dl" .
-docker run -it --rm \
+docker run -t --rm \
            -v "$PWD:/app" "openwhyd-pl-dl" \
            "/bin/bash" -c "cd /app; ./openwhyd-pl-dl-json.sh ${PROFILE_URL}"
 
