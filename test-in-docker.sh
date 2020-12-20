08b13677e3fe30e53756aa6e83257dd1bbe2143a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-20 15:11:14 +0100

    refacto test

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -3,30 +3,26 @@ set -e # this script will exit if any command returns a non-null value
 DEST_DIR="./tmp"
 docker build --tag "openwhyd-pl-dl" .
 
+function test {
+  CMD="$1"
+  EXPECTED_FILE="$2"
+  # run test
+  mkdir -p "${DEST_DIR}"
+  docker run -t --rm -v "${PWD}:/app" -w "/app" "openwhyd-pl-dl" "/bin/bash" -c "${CMD}"
+  # check expectation
+  [ ! -f "${EXPECTED_FILE}" ] && echo "❌ Missing: ${EXPECTED_FILE}" && exit 1
+  # cleanup
+  rm -rf "${DEST_DIR}"
+}
+
 # 1. Test openwhyd-pl-dl-json.sh
 
 PROFILE_URL="https://openwhyd.org/test"
-
-mkdir -p "${DEST_DIR}"
-
-docker run -t --rm -v "${PWD}:/app" -w "/app" "openwhyd-pl-dl" \
-           "/bin/bash" -c "./openwhyd-pl-dl-json.sh ${PROFILE_URL} ${DEST_DIR}"
-
-[ ! -f "${DEST_DIR}/playlists.json" ] && echo "❌ Missing: ${DEST_DIR}/playlists.json" && exit 1
-
-rm -rf "${DEST_DIR}"
+test "./openwhyd-pl-dl-json.sh ${PROFILE_URL} ${DEST_DIR}" "${DEST_DIR}/playlists.json"
 
 # 2. Test openwhyd-dl.sh
 
 PLAYLIST_URL="https://openwhyd.org/test/playlist/2" # playlist with just 1 short mp3 file
-
-mkdir -p "${DEST_DIR}"
-
-docker run -t --rm -v "${PWD}:/app" -w "/app" "openwhyd-pl-dl" \
-           "/bin/bash" -c "./openwhyd-dl.sh ${PLAYLIST_URL} ${DEST_DIR}"
-
-[ ! -f "${DEST_DIR}/00001 - mpthreetest.mp3" ] && echo "❌ Missing: ${DEST_DIR}/00001 - mpthreetest.mp3" && exit 1
-
-rm -rf "${DEST_DIR}"
+test "./openwhyd-dl.sh ${PLAYLIST_URL} ${DEST_DIR}" "${DEST_DIR}/00001 - mpthreetest.mp3"
 
 echo "✅ Done."
