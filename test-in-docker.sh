91f35329012da955e3ffc0d70e2eaf17101b8d6a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-20 14:32:47 +0100

    simplify test-in-docker.sh

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -3,8 +3,7 @@ set -e # this script will exit if any command returns a non-null value
 PROFILE_URL="https://openwhyd.org/test"
 
 docker build --tag "openwhyd-pl-dl" .
-docker run -t --rm \
-           -v "$PWD:/app" "openwhyd-pl-dl" \
-           "/bin/bash" -c "cd /app; ./openwhyd-pl-dl-json.sh ${PROFILE_URL}"
+docker run -t --rm -v "$PWD:/app" -w "/app" "openwhyd-pl-dl" \
+           "/bin/bash" -c "./openwhyd-pl-dl-json.sh ${PROFILE_URL}"
 
 echo "✅ Done."
